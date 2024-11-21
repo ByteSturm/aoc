68009ce4083bf60d7c9f43047b8de58bb9e419d3
ByteSturm <mrstormie+github@gmail.com>
2024-11-21 23:17:23 +0100

    Solving 2015/04 part 2

--- 2015/04/puzzle_201504.py
+++ 2015/04/puzzle_201504.py
@@ -2,19 +2,23 @@ import hashlib
 from puzzle_201504_input import puzzleInput
 
 
-def getLowestNumber(input: str):
+def getLowestNumberForHashWithFiveZeroes(input: str):
+    return getLowestNumberForHashStartingWith(input, "00000")
+
+
+def getLowestNumberForHashWithSixZeroes(input: str):
+    return getLowestNumberForHashStartingWith(input, "000000")
+
+
+def getLowestNumberForHashStartingWith(input: str, startCriteria: str):
     idx = 0
     digestString = ""
-    while not digestString.startswith("00000"):
+    while not digestString.startswith(startCriteria):
         idx += 1
         stringToHash = f"{input}{idx}"
         digestString = hashlib.md5(stringToHash.encode()).hexdigest()
     return idx
 
 
-def do_puzzle_part2(input: str):
-    return None
-
-
-print(getLowestNumber(puzzleInput))
-print(do_puzzle_part2(puzzleInput))
+print(getLowestNumberForHashWithFiveZeroes(puzzleInput))
+print(getLowestNumberForHashWithSixZeroes(puzzleInput))
--- 2015/04/puzzle_201504_test.py
+++ 2015/04/puzzle_201504_test.py
@@ -1,15 +1,10 @@
 import pytest
-from puzzle_201504 import getLowestNumber, do_puzzle_part2
+from puzzle_201504 import getLowestNumberForHashWithFiveZeroes
 from puzzle_201504_input import puzzleInput
 
 
 @pytest.mark.parametrize(
     "input, output", [("abcdef", 609043), ("pqrstuv", 1048970), (puzzleInput, 282749)]
 )
-def test_do_puzzle_part1(input, output):
-    assert getLowestNumber(input) == output
-
-
-@pytest.mark.parametrize("input, output", [("dummy-input", 0), (puzzleInput, None)])
-def test_do_puzzle_part2(input, output):
-    assert do_puzzle_part2(input) == output
+def test_getLowestNumber(input, output):
+    assert getLowestNumberForHashWithFiveZeroes(input) == output
