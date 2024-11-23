e8682d43cc2fd9c2048042edf9152e5bab5a4b06
ByteSturm <mrstormie+github@gmail.com>
2024-11-23 23:35:05 +0100

    Solving 2015/07 part 1

---
+++ 2015/07/puzzle_201507.py
@@ -0,0 +1,194 @@
+import re
+from enum import Enum
+from typing import Self, Union
+from dataclasses import dataclass
+from puzzle_201507_input import puzzleInput
+
+
+class Operation(Enum):
+    AND = "AND"
+    OR = "OR"
+    NOT = "NOT"
+    LSHIFT = "LSHFIT"
+    RSHIFT = "RSHIFT"
+
+
+class Wire:
+    identifier: str
+    input: int | Self | "Gate" | None
+
+    def __init__(self, identifier: str, input: int | Self | "Gate" | None = None):
+        self.identifier = identifier
+        self.input = input
+
+    def signal(self) -> int:
+        if isinstance(self.input, Gate):
+            return self.input.output()
+        elif isinstance(self.input, Wire):
+            return self.input.signal()
+        else:
+            return self.input
+
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, Wire):
+            return False
+        return self.identifier == value.identifier and self.input == value.input
+
+
+class Gate:
+    operation: Operation
+    input1: Wire
+    input2: Union[Wire, int, None] = None
+    result: int | None
+
+    def __init__(
+        self,
+        operation: Operation,
+        input1: Union["Wire", int],
+        input2: Union["Wire", int, None] = None,
+    ):
+        self.input1 = input1
+        self.input2 = input2
+        self.operation = operation
+        self.result = None
+
+    def output(self) -> int:
+        if self.result is not None:
+            return self.result
+        if isinstance(self.input1, Wire):
+            value1 = self.input1.signal()
+        else:
+            value1 = self.input1
+        if isinstance(self.input2, Wire):
+            value2 = self.input2.signal()
+        else:
+            value2 = self.input2
+        if self.operation == Operation.AND:
+            self.result = value1 & value2
+        elif self.operation == Operation.OR:
+            self.result = value1 | value2
+        elif self.operation == Operation.NOT:
+            self.result = ~value1 & 0xFFFF
+        elif self.operation == Operation.RSHIFT:
+            self.result = value1 >> value2
+        elif self.operation == Operation.LSHIFT:
+            self.result = value1 << value2
+        else:
+            self.result = value1
+        return self.result
+
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, Gate):
+            return False
+        return (
+            self.operation == value.operation
+            and self.input1 == value.input1
+            and self.input2 == value.input2
+        )
+
+
+@dataclass
+class ParsedInput:
+    outputName: str
+    inputNameOrValue1: str | None = None
+    operationName: Operation | None = None
+    inputNameOrValue2: str | None = None
+
+
+class Circuit:
+    circuit: dict[str, Wire] = dict()
+
+    def parseLine(input: str) -> ParsedInput:
+        input1Regex = r"(?P<input1>[a-z0-9]+)?"
+        input2Regex = r"(?P<input2>[a-z0-9]+)?"
+        operationRegex = r"(?P<operation>AND|OR|RSHIFT|LSHIFT|NOT)?"
+        outputRegex = r"(?P<output>[a-z]+)"
+        captureGroups = re.search(
+            input1Regex
+            + " ?"
+            + operationRegex
+            + " ?"
+            + input2Regex
+            + " -> "
+            + outputRegex,
+            input,
+        )
+        operationEnum = None
+        if captureGroups.groupdict()["operation"] is not None:
+            operationEnum = Operation[captureGroups.groupdict()["operation"]]
+        parsedLine = ParsedInput(
+            captureGroups.groupdict()["output"],
+            captureGroups.groupdict()["input1"],
+            operationEnum,
+            captureGroups.groupdict()["input2"],
+        )
+        return parsedLine
+
+    def createCircuit(input: list[str]) -> Self:
+        instance = Circuit()
+        for line in input:
+            parsedLine = Circuit.parseLine(line)
+            outputWire = instance.circuit.setdefault(
+                parsedLine.outputName, Wire(parsedLine.outputName)
+            )
+            if parsedLine.operationName is None:
+                try:
+                    outputWire.input = int(parsedLine.inputNameOrValue1)
+                except ValueError:
+                    outputWire.input = instance.circuit.setdefault(
+                        parsedLine.inputNameOrValue1, Wire(parsedLine.inputNameOrValue1)
+                    )
+            else:
+                outputWire.input = instance.connectNewGate(parsedLine)
+            instance.circuit[parsedLine.outputName] = outputWire
+        return instance
+
+    def connectNewGate(self, parsedLine: ParsedInput) -> Gate:
+        newGate: Gate = None
+        if parsedLine.operationName == Operation.NOT:
+            inputWire = self.circuit.setdefault(
+                parsedLine.inputNameOrValue2, Wire(parsedLine.inputNameOrValue2)
+            )
+            newGate = Gate(parsedLine.operationName, inputWire)
+        elif (
+            parsedLine.operationName == Operation.AND
+            or parsedLine.operationName == Operation.OR
+        ):
+            if parsedLine.inputNameOrValue1.isdigit():
+                input1 = int(parsedLine.inputNameOrValue1)
+            else:
+                input1 = self.circuit.setdefault(
+                    parsedLine.inputNameOrValue1, Wire(parsedLine.inputNameOrValue1)
+                )
+            if parsedLine.inputNameOrValue2.isdigit():
+                input2 = int(parsedLine.inputNameOrValue2)
+            else:
+                input2 = self.circuit.setdefault(
+                    parsedLine.inputNameOrValue2, Wire(parsedLine.inputNameOrValue2)
+                )
+            newGate = Gate(parsedLine.operationName, input1, input2)
+        elif (
+            parsedLine.operationName == Operation.RSHIFT
+            or parsedLine.operationName == Operation.LSHIFT
+        ):
+            inputWire1 = self.circuit.setdefault(
+                parsedLine.inputNameOrValue1, Wire(parsedLine.inputNameOrValue1)
+            )
+            inputWire2 = int(parsedLine.inputNameOrValue2)
+            newGate = Gate(parsedLine.operationName, inputWire1, inputWire2)
+        return newGate
+
+    def executeCircuit(self) -> dict[str, int]:
+        result: dict[str, int] = dict()
+        for wire in self.circuit.values():
+            result[wire.identifier] = wire.signal()
+        return result
+
+
+def do_puzzle_part2(input):
+    return None
+
+
+if __name__ == "__main__":
+    print(Circuit.createCircuit(puzzleInput).circuit["a"].signal())
+    print(do_puzzle_part2(puzzleInput))
---
+++ 2015/07/puzzle_201507_test.py
@@ -0,0 +1,145 @@
+import pytest
+from puzzle_201507 import (
+    do_puzzle_part2,
+    Operation,
+    Gate,
+    Wire,
+    Circuit,
+    ParsedInput,
+)
+from puzzle_201507_input import puzzleInput
+
+
+@pytest.mark.parametrize(
+    "input, output",
+    [
+        (
+            [
+                "NOT x -> h",
+                "NOT y -> i",
+                "123 -> x",
+                "456 -> y",
+                "x AND y -> d",
+                "x OR y -> e",
+                "x LSHIFT 2 -> f",
+                "y RSHIFT 2 -> g",
+            ],
+            {
+                "d": 72,
+                "e": 507,
+                "f": 492,
+                "g": 114,
+                "h": 65412,
+                "i": 65079,
+                "x": 123,
+                "y": 456,
+            },
+        ),
+        (puzzleInput, None),
+    ],
+)
+def test_executeCircuit(input, output):
+    createdCircuit = Circuit.createCircuit(input)
+    result = createdCircuit.executeCircuit()
+    assert result == output
+
+
+@pytest.mark.parametrize("input, output", [("dummy-input", 0), (puzzleInput, None)])
+def test_do_puzzle_part2(input, output):
+    assert do_puzzle_part2(input) == output
+
+
+def test_simpleWire():
+    simpleWire = Wire("simpleWire", 2)
+    assert simpleWire.identifier == "simpleWire"
+    assert simpleWire.signal() == 2
+
+
+def test_wireToWire():
+    inputWire = Wire("simpleWire", 5)
+    wireToWire = Wire("wireToWire", inputWire)
+    assert wireToWire.identifier == "wireToWire"
+    assert wireToWire.signal() == 5
+
+
+def test_andGate():
+    inputWire1 = Wire("inputWire", 3)
+    inputWire2 = Wire("inputWire", 5)
+    gate = Gate(Operation.AND, inputWire1, inputWire2)
+    assert gate.output() == 1
+
+
+def test_orGate():
+    inputWire1 = Wire("inputWire", 3)
+    inputWire2 = Wire("inputWire", 4)
+    gate = Gate(Operation.OR, inputWire1, inputWire2)
+    assert gate.output() == 7
+
+
+def test_notGate():
+    inputWire = Wire("inputWire", 3)
+    gate = Gate(Operation.NOT, inputWire)
+    assert gate.output() == 65532
+
+
+def test_rightShiftGate():
+    inputWire = Wire("inputWire", 3)
+    gate = Gate(Operation.RSHIFT, inputWire, 2)
+    assert gate.output() == 0
+
+
+def test_leftShiftGate():
+    inputWire = Wire("inputWire", 3)
+    gate = Gate(Operation.LSHIFT, inputWire, 2)
+    assert gate.output() == 12
+
+
+@pytest.mark.parametrize(
+    "input, output",
+    [
+        ("123 -> x", ParsedInput("x", inputNameOrValue1="123")),
+        ("456 -> y", ParsedInput("y", inputNameOrValue1="456")),
+        ("x AND y -> d", ParsedInput("d", "x", Operation.AND, "y")),
+        ("x OR y -> e", ParsedInput("e", "x", Operation.OR, "y")),
+        ("x LSHIFT 2 -> f", ParsedInput("f", "x", Operation.LSHIFT, "2")),
+        ("y RSHIFT 2 -> g", ParsedInput("g", "y", Operation.RSHIFT, "2")),
+        ("NOT x -> h", ParsedInput("h", None, Operation.NOT, "x")),
+        ("NOT y -> i", ParsedInput("i", None, Operation.NOT, "y")),
+    ],
+)
+def test_parseLine(input, output):
+    assert Circuit.parseLine(input) == output
+
+
+def test_createCircuit():
+    input = [
+        "NOT x -> h",
+        "NOT y -> i",
+        "123 -> x",
+        "456 -> y",
+        "x AND y -> d",
+        "x OR y -> e",
+        "x LSHIFT 2 -> f",
+        "y RSHIFT 2 -> g",
+    ]
+    circuit = Circuit.createCircuit(input)
+
+    xWire = Wire("x", 123)
+    yWire = Wire("y", 456)
+    dWire = Wire("d", Gate(Operation.AND, xWire, yWire))
+    eWire = Wire("e", Gate(Operation.OR, xWire, yWire))
+    fWire = Wire("f", Gate(Operation.LSHIFT, xWire, 2))
+    gWire = Wire("g", Gate(Operation.RSHIFT, xWire, 2))
+    hWire = Wire("h", Gate(Operation.NOT, xWire))
+    iWire = Wire("i", Gate(Operation.NOT, yWire))
+    expectedCircuit = Circuit()
+    expectedCircuit.circuit["x"] = xWire
+    expectedCircuit.circuit["y"] = yWire
+    expectedCircuit.circuit["d"] = dWire
+    expectedCircuit.circuit["e"] = eWire
+    expectedCircuit.circuit["f"] = fWire
+    expectedCircuit.circuit["g"] = gWire
+    expectedCircuit.circuit["h"] = hWire
+    expectedCircuit.circuit["i"] = iWire
+
+    assert circuit.circuit == expectedCircuit.circuit
