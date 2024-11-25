bf16291d2b4922a40c63b434faa631ead87c3ff4
ByteSturm <mrstormie+github@gmail.com>
2024-11-25 19:56:36 +0100

    Solving 2015/14 part 1

---
+++ 2015/14/puzzle_201514.py
@@ -0,0 +1,88 @@
+from operator import attrgetter
+import re
+from puzzle_201514_input import puzzleInput
+
+
+class Reindeer:
+    name: str
+    speedPerSecond: int
+    flyDurationInSeconds: int
+    restDurationInSeconds: int
+
+    state: str
+    timeRemainingInState: int
+    distance: int
+
+    def __init__(
+        self,
+        name: str,
+        speedPerSecond: int,
+        flyDurationInSeconds: int,
+        restDurationInSeconds: int,
+    ) -> None:
+        self.name = name
+        self.speedPerSecond = speedPerSecond
+        self.flyDurationInSeconds = flyDurationInSeconds
+        self.restDurationInSeconds = restDurationInSeconds
+        self.distance = 0
+        self.state = "flying"
+        self.timeRemainingInState = flyDurationInSeconds
+
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, Reindeer):
+            return False
+        return (
+            self.name == value.name
+            and self.speedPerSecond == value.speedPerSecond
+            and self.flyDurationInSeconds == value.flyDurationInSeconds
+            and self.restDurationInSeconds == value.restDurationInSeconds
+            and self.distance == value.distance
+            and self.state == value.state
+            and self.timeRemainingInState == value.timeRemainingInState
+        )
+
+    def run(self):
+        if self.timeRemainingInState == 0:
+            if self.state == "flying":
+                self.state = "resting"
+                self.timeRemainingInState = self.restDurationInSeconds
+            elif self.state == "resting":
+                self.state = "flying"
+                self.timeRemainingInState = self.flyDurationInSeconds
+        if self.state == "flying":
+            self.distance += self.speedPerSecond
+        self.timeRemainingInState -= 1
+
+
+def firstRace(input: list[str], duration: int) -> Reindeer:
+    reindeers = parseInput(input)
+    for _ in range(duration):
+        for reindeer in reindeers:
+            reindeer.run()
+    return max(reindeers, key=attrgetter("distance"))
+
+
+def do_puzzle_part2(input):
+    return None
+
+
+def parseInput(input: list[str]) -> list[Reindeer]:
+    reindeers = list()
+    for line in input:
+        searchResult = re.search(
+            r"(?P<name>\w+) can fly (?P<speed>\d+) km\/s for (?P<flyDuration>\d+) seconds, but then must rest for (?P<restDuration>\d+) seconds",
+            line,
+        )
+        reindeer = Reindeer(
+            searchResult.group("name"),
+            int(searchResult.group("speed")),
+            int(searchResult.group("flyDuration")),
+            int(searchResult.group("restDuration")),
+        )
+        reindeers.append(reindeer)
+    return reindeers
+
+
+if __name__ == "__main__":
+    print(firstRace(puzzleInput, 2503).distance)
+    print(do_puzzle_part2(puzzleInput))
---
+++ 2015/14/puzzle_201514_test.py
@@ -0,0 +1,67 @@
+import pytest
+from puzzle_201514 import firstRace, do_puzzle_part2, parseInput, Reindeer
+from puzzle_201514_input import puzzleInput
+
+
+@pytest.mark.parametrize(
+    "input, output",
+    [
+        (
+            [
+                "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds."
+            ],
+            [Reindeer("Comet", 14, 10, 127)],
+        ),
+        (
+            [
+                "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."
+            ],
+            [Reindeer("Dancer", 16, 11, 162)],
+        ),
+    ],
+)
+def test_parseInput(input, output):
+    assert parseInput(input) == output
+
+
+@pytest.mark.parametrize(
+    "reindeer, duration, output",
+    [
+        (Reindeer("Comet", 14, 10, 127), 10, 140),
+        (Reindeer("Dancer", 16, 11, 162), 10, 160),
+        (Reindeer("Comet", 14, 10, 127), 11, 140),
+        (Reindeer("Dancer", 16, 11, 162), 11, 176),
+        (Reindeer("Dancer", 16, 11, 162), 12, 176),
+        (Reindeer("Comet", 14, 10, 127), 137, 140),
+        (Reindeer("Dancer", 16, 11, 162), 173, 176),
+        (Reindeer("Comet", 14, 10, 127), 138, 154),
+        (Reindeer("Dancer", 16, 11, 162), 174, 192),
+        (Reindeer("Comet", 14, 10, 127), 1000, 1120),
+        (Reindeer("Dancer", 16, 11, 162), 1000, 1056),
+    ],
+)
+def test_reindeers(reindeer, duration, output):
+    for _ in range(duration):
+        reindeer.run()
+    assert reindeer.distance == output
+
+
+@pytest.mark.parametrize(
+    "input, duration, winnerName, winnerDistance",
+    [
+        (
+            [
+                "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.",
+                "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.",
+            ],
+            1000,
+            "Comet",
+            1120,
+        ),
+        (puzzleInput, 2503, "Donner", 2655),
+    ],
+)
+def test_firstRace(input, duration, winnerName, winnerDistance):
+    winner = firstRace(input, duration)
+    assert winner.name == winnerName
+    assert winner.distance == winnerDistance
