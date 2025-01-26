9315c43bf9ac905e1ee25ee4b282caa0ccfd35b5
ByteSturm <mrstormie+github@gmail.com>
2025-01-26 11:02:48 +0100

    Removed unused testcase

--- 2015/20/puzzle_201520_test.py
+++ 2015/20/puzzle_201520_test.py
@@ -1,31 +1,9 @@
-import time
 import pytest
 from puzzle_201520 import (
-    # build_house_present_map,
     do_puzzle_part1,
     do_puzzle_part2,
 )
 from puzzle_201520_input import puzzleInput
-
-
-# @pytest.mark.parametrize(
-#     "input, output",
-#     [
-#         (1, 10),
-#         (2, 30),
-#         (3, 40),
-#         (4, 70),
-#         (5, 60),
-#         (6, 120),
-#         (7, 80),
-#         (8, 150),
-#         (9, 130),
-#         (puzzleInput, None),  # 831600
-#     ],
-# )
-# def test_sum_with(input, output):
-#     factors = build_house_present_map(input)
-#     assert sum_with(factors[input], lambda x: x * 10) == output
 
 
 @pytest.mark.parametrize(
